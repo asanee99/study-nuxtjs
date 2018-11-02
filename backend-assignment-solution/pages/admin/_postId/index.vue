0e530e103a2bef4aba5dfa0117cdc35402b02da2
ASANEE <asanee.tan@gmail.com>
2018-11-02 13:34:06 +0700

    Finish learn about nuxtjs

---
+++ backend-assignment-solution/pages/admin/_postId/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "@/components/Admin/AdminPostForm";
+import axios from "axios";
+
+export default {
+  layout: "admin",
+  components: {
+    AdminPostForm
+  },
+  asyncData(context) {
+    return axios
+      .get(
+        "https://nuxt-blog.firebaseio.com/posts/" +
+          context.params.postId +
+          ".json"
+      )
+      .then(res => {
+        return {
+          loadedPost: res.data
+        }
+      })
+      .catch(e => context.error());
+  }
+};
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
